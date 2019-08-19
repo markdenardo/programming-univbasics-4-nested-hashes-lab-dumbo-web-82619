cb981179b9d880c0ac5839cd54b61bf19ac08f0d
Mark DeNardo <mark.denardo@gmail.com>
2019-08-19 06:48:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,9 +70,9 @@ def adding_matz
 
 	programmer_hash = 
  		{     
- 		      :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+ 		      :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
